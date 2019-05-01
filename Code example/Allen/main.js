43090019a1a9ca8b3ab4d1acee1d80a91895ddcb
Allen <awong503@my.bcit.ca>
2019-04-30 21:10:20 -0700

    removed gravity

--- Code example/Allen/main.js
+++ Code example/Allen/main.js
@@ -23,31 +23,35 @@ var game = new Phaser.Game(config);
 function preload ()
 {
     this.load.image('bg', 'images/ocean.png');
-    this.load.image('ship', 'images/turtle.gif');
+    this.load.image('turtle', 'images/turtle.gif');
     this.load.image('garbage', 'images/garbage.gif')
 }
 
 function create ()
 {
     //  Set the camera and physics bounds to be the size of 4x4 bg images
-    this.cameras.main.setBounds(0, 0, 1697 * 2, 1191 * 2);
-    this.physics.world.setBounds(0, 0, 1697 * 2, 1191 * 2);
+    this.cameras.main.setBounds(0, 0, 1697, 1191);
+    this.physics.world.setBounds(0, 0, 1697, 1191);
     this.physics.world.gravity.y = 60;
 
     //  Mash 4 images together to create our background
     this.add.image(0, 0, 'bg').setOrigin(0);
     this.add.image(1697, 0, 'bg').setOrigin(0).setFlipX(true);
-    this.add.image(0, 1191, 'bg').setOrigin(0).setFlipY(true);
-    this.add.image(1697, 1191, 'bg').setOrigin(0).setFlipX(true).setFlipY(true);
+    // this.add.image(0, 1191, 'bg').setOrigin(0).setFlipY(true);
+    // this.add.image(1697, 1191, 'bg').setOrigin(0).setFlipX(true).setFlipY(true);
 
-    cursors = this.input.keyboard.createCursorKeys();
 
-    player = this.physics.add.image(400, 300, 'ship');
+    player = this.physics.add.image(128, 128, 'turtle');
     this.cameras.main.startFollow(player);
     this.cameras.main.followOffset.set(-300, 0);
 
     player.setCollideWorldBounds(true);
-    player.create(400,300).setGravity(0,-120);
+    // player.create(400,300).setGravity(0,-120);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+
+
 
 
 
@@ -78,4 +82,6 @@ function update ()
     {
         player.setVelocityY(500);
     }
+
+
 }
