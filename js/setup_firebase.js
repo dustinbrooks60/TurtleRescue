4715147b3f0ac4b3cfcbf55dde5ffc84d3dca891
TrungYU <quoctrung1220@gmail.com>
2019-05-09 12:41:13 -0700

    added login functions and setup firebase

---
+++ js/setup_firebase.js
@@ -0,0 +1,14 @@
+(function() {
+    // Web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyAfyxJ1IN7AtVGyRCsQHzA-jxUtdILUPTs",
+        authDomain: "shellshocked2930.firebaseapp.com",
+        databaseURL: "https://shellshocked2930.firebaseio.com",
+        projectId: "shellshocked2930",
+        storageBucket: "shellshocked2930.appspot.com",
+        messagingSenderId: "24641943764",
+        appId: "1:24641943764:web:53176a886dc069ce"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+})();
