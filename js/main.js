b187368b9ceb35f74a627195fff9c8449033735e
Allen <awong503@my.bcit.ca>
2019-05-14 10:27:07 -0700

    Merge branch 'develop' of C:\Users\Allen\Desktop\Team-07-COMP-2930 with conflicts.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,124 @@
-// Initialize variables
 let ctx;
 let turtle;
 let oceanBackground;
-let enemies;
-let garbageArr;
+let enemies = [];
+let garbageArr = [];
 let garbageClump;
 let displayScore;
 let displayMultiplier;
 let score;
-let multiplier;
+let multiplier = 1;
 let topHat;
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+let seconds;
+let barValue;
+let userChoice;
+let dbQuestion;
 
-// Create main elements for game
-function startGame() {
-    resetGame();
-    turtle = new Element(9600, 600, './images/turtle-sprite2.png', 10, 120, "sprite", 12); // turtle object
-    turtle.gravity = 0.08;
-    oceanBackground = new Element(1800, window.innerHeight, './images/ocean_2.png', 0, 0, "background"); // game background
-    displayScore = new Element("20px", "Consolas", "black", 10, 30, "text");
-    displayMultiplier = new Element("20px", "Consolas", "black", 10, 60, "text");
-    pullQuestion();
-    gameCanvas.start(); // appends game canvas to the body
+let question = document.getElementById('question');
+let answer1 = document.getElementById('a1');
+let answer2 = document.getElementById('a2');
+let answer3 = document.getElementById('a3');
+let answer4 = document.getElementById('a4');
+
+let questionKey;
+let answerObject;
+let answerArray;
+
+let copyAnswerArray;
+
+// Pull random question from database
+function getQuestion() {
+    let num = Math.ceil(Math.random() * 15);
+    let dbRef = firebase.database().ref('/questions/' + 'question' + num);
+    dbRef.once('value').then(function(snapshot){
+        dbQuestion = snapshot.val();
+    });
+    return dbQuestion
 }
 
-// Reset game elements for a new round of game play
-function resetGame() {
+function generateQuestion(){
+    getQuestion();
+    questionKey = Object.keys(dbQuestion); // questionKey length 1 array of question type:string
+    answerObject = dbQuestion[questionKey];
+    answerArray = Object.keys(answerObject); // answerArray contain answers of type string
+}
+
+function getAnswer(answerObj, answerArray){
+    // gets the correct answer
+    for(let i = 0; i < answerArray.length; i++){
+        let find_answer = answerObj[answerArray[i]];
+        if (find_answer === true){
+            return answerArray[i]
+        }
+    }
+}
+function shuffle(array){
+    // randomly sort trivia answers
+    array.sort(() => Math.random() - 0.5);
+}
+function displayTrivia(){
+    // display trivia for user to answer
+    copyAnswerArray = answerArray.slice(0); // create a copy of the questions
+    shuffle(copyAnswerArray); // shuffle the answers
+    question.innerHTML = questionKey[0]; // index can be the length of how many questions to randomize
+    answer1.innerHTML = copyAnswerArray[0];
+    answer2.innerHTML = copyAnswerArray[1];
+    answer3.innerHTML = copyAnswerArray[2];
+    answer4.innerHTML = copyAnswerArray[3];
+    isCorrectAnswer();
+
+}
+
+function isCorrectAnswer(){
+    // should return boolean if user chooses a correct answer
+    checkUserChoice(); // check for correct user selection
+    setTimeout(function(){
+        if (userChoice === getAnswer(answerObject, copyAnswerArray)){
+            console.log("correct answer!") // do this if user choice is correct
+        } else{
+            console.log("wrong!")
+        }
+    },5000); // timeout determine by countDown interval
+
+
+}
+// get the innerHTML value and assign to userChoice to check if answer is correct
+function checkUserChoice(){
+    answer1.onclick = function(){userChoice = answer1.innerHTML};
+    answer2.onclick = function(){userChoice = answer2.innerHTML};
+    answer3.onclick = function(){userChoice = answer3.innerHTML};
+    answer4.onclick = function(){userChoice = answer4.innerHTML};
+
+    return userChoice
+}
+
+function resetTrivia(){
+    question.innerHTML = ""; // index can be the length of how many questions to randomize
+    answer1.innerHTML = "";
+    answer2.innerHTML = "";
+    answer3.innerHTML = "";
+    answer3.innerHTML = "";
+}
+function countDown(intervalSec){
+    // create a countdown for user with a progress bar.
+    seconds = intervalSec;
+    let x = setInterval(function(){
+        barValue = (seconds-1)*20;
+        document.getElementById('time').style.width = String(barValue) + "%";
+        // document.getElementById('time').innerHTML = seconds-1 + "seconds left";
+        if (seconds === 0){
+            document.getElementById('time').style.width = String(100) + "%";
+            // document.getElementById('time').innerHTML = 5 + "seconds left";
+            clearInterval(x);
+
+        }else {
+            seconds -= 1;
+        }
+    }, 1000)
+}
+function startGame() {
     score = 0;
     multiplier = 1;
     enemies = [];
@@ -34,38 +128,169 @@ function resetGame() {
     document.getElementById("start").style.display = "none";
     document.getElementById("restart").style.display = "none";
     document.getElementById("trivia").style.display = "none";
+    turtle = new Element(9600, 600, './images/turtle-sprite2.png', 10, 120, "sprite", 12); // turtle object
+    turtle.gravity = 0.08;
+    oceanBackground = new Element(1800, windowHeight, './images/ocean_2.png', 0, 0, "background"); // game background
+    displayScore = new Element("20px", "Consolas", "black", 10, 30, "text");
+    displayMultiplier = new Element("20px", "Consolas", "black", 10, 60, "text");
+    gameCanvas.start(); // appends game canvas to the body
 }
 
-// Creates the game canvas object
+// creates the game canvas object
 let gameCanvas = {
     canvas : document.createElement("canvas"),
     // Sets game canvas dimensions and appends it to the body
     start : function() {
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
+        getQuestion();
+        this.canvas.width = windowWidth;
+        this.canvas.height = windowHeight;
         this.context = this.canvas.getContext("2d");
         document.body.appendChild(this.canvas);
+        // document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
-        this.interval = setInterval(updateGameCanvas, 15); // clears and redraws game canvas every 15ms
+        this.interval = setInterval(updateGameArea, 15); // clears and redraws game canvas every 15ms
     },
     // Clears the contents of the game canvas
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
-    // Stops updating the game canvas
+    // Stops updating the game canvas (when character collides with enemy)
     stop : function() {
         clearInterval(this.interval);
     },
     continue : function(){
-        pullQuestion(); // pull new question after round of trivia
-        this.interval = setInterval(updateGameCanvas, 15);
+        getQuestion();
+        this.interval = setInterval(updateGameArea, 15);
 
     }
 };
 
+function Element(width, height, color, x, y, type, num_frames) {
+    this.type = type;
+    if (type === "image" || type === "background" || type === "sprite") {
+        this.image = new Image();
+        this.image.src = color;
+    }
+    this.actual_width = width;
+    this.actual_height = height;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.gravity = 0;
+    this.gravitySpeed = 0;
+    this.x = x;
+    this.y = y;
+    this.num_frames = num_frames; // number of frames in the sprite image
+    this.sprite_frame = 0; // the particular frame number of the sprite image
+    // redraws the all the game canvas, as well as all the elements in it
+    this.update = function() {
+        ctx = gameCanvas.context;
+        if (type === "text") {
+            ctx.font = this.width + " " + this.height;
+            ctx.fillStyle = color;
+            ctx.fillText(this.text, this.x, this.y);
+        }
+        else if (type === "image" || type === "background") {
+            ctx.drawImage(this.image,
+                this.x,
+                this.y,
+                this.width,
+                this.height);
+            if (type === "background") {
+                ctx.drawImage(this.image,
+                    this.x + this.width,
+                    this.y,
+                    this.width,
+                    this.height);
+            }
+        }
+        else if (type === "sprite") {
+            this.actual_width = (this.width / this.num_frames)/4;
+            this.actual_height = this.height /4;
+            ctx.drawImage(this.image,
+                (this.sprite_frame * (this.width / this.num_frames)),
+                0,
+                this.width / this.num_frames,
+                this.height,
+                this.x,
+                this.y,
+                this.actual_width,
+                this.actual_height);
+
+            // set frame rate of sprite animations
+            if (gameCanvas.frameNo % 6 === 0) {
+                if (this.sprite_frame === this.num_frames - 1) {
+                    this.sprite_frame = 0; // restarts the sprite animation
+                }
+                else {
+                    this.sprite_frame++; // moves to the next sprite frame
+                }
+            }
+        }
+        else {
+            // draws a rectangle if sprite image is unavailable
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    };
+    // Update the position of all the objects in the canvas
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY + this.gravitySpeed;
+        if (this.type === 'image' || this.type === "sprite") {
+            this.gravitySpeed += this.gravity;
+            if (this.gravitySpeed <-3) { // caps gravity at -3
+                this.gravitySpeed = -3
+            }
+            this.hitBottom();
+            this.hitTop();
+        }
+        if (this.type === "background") {
+            if (this.x === -(this.width)) {
+                this.x = 0;
+            }
+        }
+    };
+    // Detects if turtle hits the ocean floor
+    this.hitBottom = function() {
+        let oceanFloor = gameCanvas.canvas.height - this.height + 425;
+        if (this.y > oceanFloor) {
+            this.y = oceanFloor;
+            this.gravitySpeed = 0;
+        }
+        accelerate(0.1);
+    };
+    // Detects if the turtle hits the ocean surface
+    this.hitTop = function() {
+        let oceanSurface = 80;
+        if (this.y < oceanSurface){
+            this.y = oceanSurface;
+            this.gravitySpeed = 0;
+        }
+    };
+    // Sets the hit boxes of turtle, enemies, and garbage and detects if they collide
+    this.crashWith = function(otherobj) {
+        let turtleLeft = this.x;
+        let turtleRight = this.x + (this.actual_width);
+        let turtleTop = this.y;
+        let turtleBottom = this.y + (this.actual_height);
+        let enemyLeft = otherobj.x + 85;
+        let enemyRight = otherobj.x + (otherobj.actual_width) - 85;
+        let enemyTop = otherobj.y + 85;
+        let enemyBottom = otherobj.y + (otherobj.actual_height) - 75;
+        let crash = true;
+        if ((turtleBottom < enemyTop) || (turtleTop > enemyBottom) || (turtleRight < enemyLeft) || (turtleLeft > enemyRight)) {
+            crash = false;
+        }
+        return crash; // Returns boolean status of a collision between a turtle and other object
+    };
+}
+
 // Accelerates turtle upon screen touch
 function clickManager(n){
     accelerate(n);
+    console.log('clicked');
 }
 
 function accelerate(n){
@@ -88,81 +313,63 @@ function clearNearbyEnemies() {
 }
 
 // Redraws the game canvas and all elements in it
-function updateGameCanvas() {
-    checkCollision();
-    gameCanvas.clear();
-    gameCanvas.frameNo += 1;
-    oceanBackground.speedX = -1;
-    oceanBackground.updatePosition();
-    oceanBackground.draw();
-    addObjects();
-    drawElements();
-    displayScore.text = "Garbage Collected:" + score;
-    displayMultiplier.text = "Multiplier: x" + multiplier;
-    displayScore.draw();
-    displayMultiplier.draw();
-}
-
-function drawElements() {
-    // Updates enemy positions and draws them on the game canvas
+function updateGameArea() {
     for (let i = 0; i < enemies.length; i++) {
-        enemies[i].x += -1;
-        enemies[i].draw();
-    }
-    // Updates garbage positions and draws them on the game canvas
-    for (let i = 0; i < garbageArr.length; i++) {
-        garbageArr[i].x += -1;
-        garbageArr[i].draw();
-    }
-    // Updates hat position and draws it on the game canvas
-    if (topHat) {
-        topHat.x += -1;
-        topHat.draw();
-    }
-    // Updates garbage clump position and draws it on the game canvas
-    if (garbageClump) {
-        garbageClump.x += -1;
-        garbageClump.draw();
-    }
-    turtle.updatePosition();
-    turtle.draw();
-}
-
-function checkCollision() {
-    for (let i = 0; i < enemies.length; i++) {
-        // Check if turtle has collided with enemy
-        if (turtle.collidesWith(enemies[i])) {
-            gameCanvas.stop();
-            scoreToDB();
-            document.getElementById('restart').style = "display: flex; z-index: 10";
-            document.getElementById("score").innerHTML = "Score: " + score;
-        }
-        // remove enemy from array once it has gone off the screen
         if (enemies[i].x < -100) {
             enemies.splice(i, 1);
         }
     }
+
+    // Check if turtle has collided with enemy
+    for (let i = 0; i < enemies.length; i++) {
+        if (turtle.crashWith(enemies[i])) {
+            gameCanvas.stop();
+            document.getElementById('restart').style = "display: flex; z-index: 10";
+            document.getElementById("score").innerHTML = "Score: " + score;
+        }
+    }
     // Check if turtle has collided with garbage
     for (let i = 0; i < garbageArr.length; i++) {
-        if (turtle.collidesWith(garbageArr[i])) {
+        if (turtle.crashWith(garbageArr[i])) {
             garbageArr.splice(i, 1);
             score += multiplier;
         }
     }
     // Check if turtle has collided with top hat
-    if (topHat && turtle.collidesWith(topHat)) {
+    if (topHat && turtle.crashWith(topHat)) {
         topHat = false;
         turtle.image.src = './images/dapper-sprite2.png';
     }
     // Check if turtle has collided with large garbage clump
-    if (garbageClump && turtle.collidesWith(garbageClump)) {
+    if (garbageClump && turtle.crashWith(garbageClump)) {
+        garbageClump = false;
+        let trivia = document.getElementById('trivia');
+        generateQuestion();
         gameCanvas.stop();
-        startTrivia();
+        trivia.style = "display: flex; z-index: 10";
+        displayTrivia();
+        countDown(5);
+        setTimeout(function(){
+            seconds = 5;
+            trivia.style.display = "none";
+            resetTrivia();
+            garbageClump = false;
+            clearNearbyEnemies()
+        }, 6000);
+        setTimeout(function() {
+            gameCanvas.continue();
+        }, 6001);
+        multiplier ++;
+
 
     }
-}
 
-function addObjects() {
+    gameCanvas.clear();
+    gameCanvas.frameNo += 1;
+    oceanBackground.speedX = -1;
+    oceanBackground.newPos();
+    oceanBackground.update();
+
     // adds enemy sprite to the game canvas
     if (gameCanvas.frameNo === 1 || everyInterval(350)) {
         let enemyImages = ['./images/jelly-sprite2.png', './images/puffer-sprite5.png']; // enemy sprites
@@ -201,4 +408,31 @@ function addObjects() {
         let garbageY = Math.floor(Math.random() * (gameCanvas.canvas.height - 400) + 150);
         garbageClump = new Element(180, 180, './images/clump.png', x, garbageY, "image")
     }
+
+    // Updates enemy positions and draws them on the game canvas
+    for (let i = 0; i < enemies.length; i++) {
+        enemies[i].x += -1;
+        enemies[i].update();
+    }
+    // Updates garbage positions and draws them on the game canvas
+    for (let i = 0; i < garbageArr.length; i++) {
+        garbageArr[i].x += -1;
+        garbageArr[i].update();
+    }
+    // Updates hat position and draws it on the game canvas
+    if (topHat) {
+        topHat.x += -1;
+        topHat.update();
+    }
+    // Updates garbage clump position and draws it on the game canvas
+    if (garbageClump) {
+        garbageClump.x += -1;
+        garbageClump.update();
+    }
+    displayScore.text = "Garbage Collected:" + score;
+    displayMultiplier.text = "Multiplier: x" + multiplier;
+    displayScore.update();
+    displayMultiplier.update();
+    turtle.newPos();
+    turtle.update();
 }
