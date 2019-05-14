584d48b0af3142c705e02910be8842c77d95b6d8
TrungYU <quoctrung1220@gmail.com>
2019-05-13 18:32:23 -0700

    Added firebase_highscore.js

---
+++ js/firebase_highscore.js
@@ -0,0 +1,52 @@
+var userName;
+var userId;
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // User is signed in.
+        userName = user.displayName;
+        var isAnonymous = user.isAnonymous;
+        userId = user.uid;
+    } else {
+        // User is signed out.
+    }
+});
+
+var scoresRoot = firebase.database().ref().child('scores');
+// Set up score in database
+var infoJSONobj = {};
+
+scoresRoot.on('value', (snapshot) => {
+    let playerData = snapshot.val();
+    if (playerData[userId] === undefined) {
+        infoJSONobj.bestScore = 0;
+        infoJSONobj.userName = userName;
+        firebase.database().ref().child('scores').child(userId).set(infoJSONobj);
+
+    }
+    else {
+        infoJSONobj.bestScore = playerData[userId].bestScore;
+    }
+});
+
+// Top score grabber:
+scoresRoot.on('value', (snapshot) => {
+    let playerData = snapshot.val();
+    var highestScore = -10000;
+    var highestUser = "";
+    Object.keys(playerData).forEach(function(k){
+        if (playerData[k].bestScore > highestScore){
+            highestScore = playerData[k].bestScore; highestUser = playerData[k].userName;
+        }
+    });
+    console.log("Top score is " + highestScore+" by "+highestUser);
+});
+
+function scoreToDB(){
+    if (infoJSONobj.bestScore < score) {
+        infoJSONobj.bestScore = score;
+        infoJSONobj.userName = userName;
+        firebase.database().ref().child('scores').child(userId).set(infoJSONobj);
+    }
+    console.log('You got ' + infoJSONobj.bestScore);
+}
