2dd17373b0a9438770d6be4ad2039c5442227b72
TrungYU <quoctrung1220@gmail.com>
2019-05-17 16:41:42 -0700

    Updated firebase_api.js

--- js/firebase_api.js
+++ js/firebase_api.js
@@ -1,13 +1,13 @@
 (function() {
     // Web app's Firebase configuration
     var firebaseConfig = {
-        apiKey: "AIzaSyAfyxJ1IN7AtVGyRCsQHzA-jxUtdILUPTs",
-        authDomain: "shellshocked2930.firebaseapp.com",
-        databaseURL: "https://shellshocked2930.firebaseio.com",
-        projectId: "shellshocked2930",
-        storageBucket: "shellshocked2930.appspot.com",
-        messagingSenderId: "24641943764",
-        appId: "1:24641943764:web:53176a886dc069ce"
+        apiKey: "AIzaSyBeieI2MvuRNC-csEmmh-3BzViSt6C62Os",
+        authDomain: "turtlerescue2930.firebaseapp.com",
+        databaseURL: "https://turtlerescue2930.firebaseio.com",
+        projectId: "turtlerescue2930",
+        storageBucket: "turtlerescue2930.appspot.com",
+        messagingSenderId: "769435923901",
+        appId: "1:769435923901:web:a113e8ee1f3b36ab"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
