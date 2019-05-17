225bf7e335d510433ae7497f85cff6a681a3ee82
Allen <awong503@my.bcit.ca>
2019-05-17 14:25:44 -0700

    updated with required token function

--- js/firebaseauthentication.js
+++ js/firebaseauthentication.js
@@ -2,25 +2,7 @@ var provider = new firebase.auth.GoogleAuthProvider(); // create an instance for
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 var uiConfig = {
-    googleAuth: function(){
-        firebase.auth().signInWithPopup(provider).then(function(result) {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = result.credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-            // ...
-        }).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
 
-        });
-    },
     callbacks: {
         signInSuccessWithAuthResult: function(authResult, redirectUrl) {
             // User successfully signed in.
@@ -35,7 +17,7 @@ var uiConfig = {
         }
     },
 // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
+    signInFlow: 'redirect',
     signInSuccessUrl: 'index.html',
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
@@ -96,6 +78,26 @@ firebase.auth().onAuthStateChanged(function(user) {
 
     }
 });
+
+firebase.auth().getRedirectResult().then(function(result) {
+    if (result.credential) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // ...
+    }
+    // The signed-in user info.
+    var user = result.user;
+}).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+});
+
 function signOut() {
     firebase.auth().signOut();
     console.log('shit')
