156e3d2823bc369a306a06b7d12f25108d49caf2
TrungYU <quoctrung1220@gmail.com>
2019-05-20 12:41:25 -0700

    Rearranged files

---
+++ developing/js/firebase_api.js
@@ -0,0 +1,14 @@
+(function() {
+    // Web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyBeieI2MvuRNC-csEmmh-3BzViSt6C62Os",
+        authDomain: "turtlerescue2930.firebaseapp.com",
+        databaseURL: "https://turtlerescue2930.firebaseio.com",
+        projectId: "turtlerescue2930",
+        storageBucket: "turtlerescue2930.appspot.com",
+        messagingSenderId: "769435923901",
+        appId: "1:769435923901:web:a113e8ee1f3b36ab"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+})();
---
+++ developing/js/firebase_highscore.js
@@ -0,0 +1,93 @@
+var userName;
+var userId;
+let leaderboardArr = [];
+let topFiveArr;
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // User is signed in.
+        userName = user.displayName;
+        var isAnonymous = user.isAnonymous;
+        userId = user.uid;
+    } else {
+        // User is signed out.
+    }
+});
+
+var scoresRoot = firebase.database().ref().child('scores');
+// Set up score in database
+var infoJSONobj = {};
+
+scoresRoot.on('value', (snapshot) => {
+    let playerData = snapshot.val();
+    if (playerData[userId] === undefined) {
+        infoJSONobj.bestScore = 0;
+        infoJSONobj.userName = userName;
+        firebase.database().ref().child('scores').child(userId).set(infoJSONobj);
+
+    }
+    else {
+        infoJSONobj.bestScore = playerData[userId].bestScore;
+    }
+});
+
+// Top score grabber:
+scoresRoot.on('value', (snapshot) => {
+    leaderboardArr = [];
+    let playerData = snapshot.val();
+    var highestScore = -10000;
+    var highestUser = "";
+    Object.keys(playerData).forEach(function(k){
+        leaderboardArr.push(playerData[k]); // create an array of the objects
+        if (playerData[k].bestScore > highestScore){
+
+            highestScore = playerData[k].bestScore; highestUser = playerData[k].userName;
+        }
+    });
+    sortScore();
+    if (leaderboardArr.length >= 5) {
+        topFiveArr = leaderboardArr.slice(leaderboardArr.length - 5); // get the top five users
+    } else {
+        topFiveArr = leaderboardArr.slice();
+    }
+    populateLeaderBoard();
+    console.log("Top score is " + highestScore+" by "+highestUser);
+
+});
+
+function scoreToDB(){
+    if (infoJSONobj.bestScore < score) {
+        try {
+            infoJSONobj.bestScore = score;
+            infoJSONobj.userName = userName;
+            firebase.database().ref().child('scores').child(userId).set(infoJSONobj);
+        }
+        catch(err) {
+            console.log(err.message);
+        }
+
+    }
+}
+
+function sortScore(){
+    leaderboardArr.sort(function(a,b){
+        return a.bestScore - b.bestScore
+    });
+}
+
+function populateLeaderBoard(){
+    topFiveArr.reverse();
+    $(document).ready(function(){
+        for (let i = 0; i < topFiveArr.length; i++){
+            $("#top" + String(i+1) + "Name").html(topFiveArr[i].userName);
+            $("#top" + String(i+1) +"Score").html(topFiveArr[i].bestScore);
+        }
+        for (let j = topFiveArr.length; j < 5; j++){ // to clear the loading if topFiveArr.length < 5
+            $("#top" + String(j+1) + "Name").html("");
+            $("#top" + String(j+1) +"Score").html("");
+        }
+    })
+}
+
+
+
