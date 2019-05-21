eb76bc1d44d188666e0354462e3462084cb51d24
TrungYU <quoctrung1220@gmail.com>
2019-05-20 18:34:16 -0700

    Made background blur during trivia and changed #game-over .modal-dialog margin to auto

--- developing/js/trivia.js
+++ developing/js/trivia.js
@@ -3,11 +3,12 @@ let userChoice;
 let randomQuestion;
 
 function startTrivia() {
+    document.querySelector('canvas').style.filter = 'blur(5px)';
     trivia = document.getElementById('trivia');
     generateQuestion();
     trivia.style = "display: flex; z-index: 10";
     displayTrivia();
-    triviaHandler(10);
+    triviaHandler(10)
 }
 
 // Trivia display
@@ -114,6 +115,7 @@ function triviaHandler(seconds){
             sleep(3000).then(() => {
                 clearTrivia();
                 trivia.style.display = "none";
+                document.querySelector('canvas').style.filter = 'none';
                 countDown();
             })
 
